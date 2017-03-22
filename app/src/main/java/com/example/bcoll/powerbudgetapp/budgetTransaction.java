425602a244177da62d9b9cd4d4ce47c92014d083
Broden Collins <bcollins.neb@hotmail.com>
2017-03-22 13:37:31 -0500

    Updated Transaction GUI

--- app/src/main/java/com/example/bcoll/powerbudgetapp/budgetTransaction.java
+++ app/src/main/java/com/example/bcoll/powerbudgetapp/budgetTransaction.java
@@ -9,6 +9,14 @@ public class budgetTransaction extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_transaction);
+
+
+
+
+
+
+
+
     }
 
 }
