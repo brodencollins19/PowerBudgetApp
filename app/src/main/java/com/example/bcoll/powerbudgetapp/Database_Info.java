be402d6497e6821453de631041c0e28975d004c1
Broden Collins <bcollins.neb@hotmail.com>
2017-03-24 10:59:35 -0500

    -Added a database script -Implemented some File IO for DB purposes

--- app/src/main/java/com/example/bcoll/powerbudgetapp/Database_Info.java
+++ app/src/main/java/com/example/bcoll/powerbudgetapp/Database_Info.java
@@ -3,16 +3,28 @@ package com.example.bcoll.powerbudgetapp;
  * Created by bcoll on 3/13/2017.
  */
 import java.util.ArrayList;
-import android.content.Context;
 
+/*
+Java drivers for sqlite interaction with Android
+ */
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQueryBuilder;
 
+/*
+  JAve utils for file IO
+ */
+import java.io.*;
+import java.util.Scanner;
+
+
+
 public class Database_Info extends SQLiteOpenHelper {
     private static final String DB_Name = "Budget_Database";
     private static final int DB_Version = 1;
+    Scanner dbFileScanner;
 
     public Database_Info (Context context) {
         super(context,DB_Name,null,DB_Version);
@@ -20,12 +32,24 @@ public class Database_Info extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db){
-
+        Scanner dbScanner = null;
+        String createTables = "";
+        try{
+            dbScanner = new Scanner(new File("res/dbScript"));
+            while(dbScanner.hasNextLine()){
+                createTables += dbScanner.nextLine();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            dbScanner.close();
+        }
+        db.execSQL(createTables);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS contacts");
+        db.execSQL("DROP TABLE IF EXISTS Users, Budget, Incentive, Container, Transaction");
         onCreate(db);
     }
 
